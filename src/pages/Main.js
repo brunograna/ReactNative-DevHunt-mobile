e099c7863404bfc7bfdb3e0f4a6d47072479f942
Bruno Garcia Rana <brunogarciarana@gmail.com>
2020-03-28 15:24:23 -0300

    Navigation to WebView

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,9 +1,10 @@
 import React, {useEffect, useState} from 'react';
-import {StyleSheet} from 'react-native';
-import MapView from 'react-native-maps';
+import {StyleSheet, Image, View, Text} from 'react-native';
+import MapView, {Marker, Callout} from 'react-native-maps';
 import { requestPermissionsAsync, getCurrentPositionAsync } from "expo-location";
 
-function Main() {
+
+function Main({ navigation }) {
     const [currentRegion, setCurrentRegion] = useState(null);
 
     useEffect(()=>{
@@ -29,14 +30,53 @@ function Main() {
     if (!currentRegion) {
         return null;
     }
-    
-    return <MapView style={styles.map} initialRegion={currentRegion}/>
+
+    return (
+        <MapView style={styles.map} initialRegion={currentRegion}>
+            <Marker coordinate={{ latitude: -22.8230793, longitude: -43.2869129}}>
+                <Image style={styles.avatar} source={{ uri:'https://avatars0.githubusercontent.com/u/41070501?s=460&u=32770585e48879a0900b6d38d7f85949c5543a2e&v=4' }} />
+
+                <Callout onPress={()=>{
+                    // Navegação
+                    navigation.navigate('Profile', { github_username: 'brunograna' });
+                }}>
+                    <View style={styles.callout}>
+                        <Text style={styles.devName}>Bruno Garcia</Text>
+                        <Text style={styles.devBio}>Analista de Sistemas</Text>
+                        <Text style={styles.devTechs}>Java, NodeJs</Text>
+                    </View>
+                </Callout>
+            </Marker>
+        </MapView>
+    );
 }
 
 const styles = StyleSheet.create({
    map: {
        flex: 1
-   }
+   },
+    avatar: {
+        width: 50,
+        height: 50,
+        borderRadius: 4,
+        borderWidth: 4,
+        borderColor: '#fff'
+    },
+    callout: {
+        width: 260,
+    },
+    devName: {
+        fontWeight: 'bold',
+        fontSize: 16,
+    },
+    devBio: {
+        color: '#666',
+        marginTop: 5,
+    },
+    devTechs: {
+        marginTop: 5
+    },
+
 });
 
 export default Main;
